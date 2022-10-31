44edf7cbd0c73d88349d21b0c18ef93cec180664
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-10-31 18:27:27 +0800

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export { observeElement } from './observeElement.js'
-export { waitElement } from './waitElement.js'
 export { el, text, nbsp, domReady } from './html.js'
 export { fetcher, Fetcher, FetcherError } from './fetcher.js'
 export { waitElement } from './waitElement.js'
