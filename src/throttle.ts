544445874ca950bb685326e7474e92898693e75a
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-10-31 13:01:16 +0800

    chore: refactor throttle

--- src/throttle.ts
+++ src/throttle.ts
@@ -1,35 +1,17 @@
-export function throttle<T extends (...args: any[]) => void>(
+export function throttle<T extends (...args: any[]) => ReturnType<T>>(
   fn: T,
-  delay = 500
-): (this: ThisParameterType<T>, ...args: Parameters<T>) => void {
-  let isThrottle = false
-  let savedArgs: typeof fn.arguments | null = null
-  let savedThis: ThisParameterType<T> | null = null
+  ms: number
+): (...args: Parameters<T>) => ReturnType<T> {
+  let isThrottle: boolean
+  let lastResult: ReturnType<T>
 
-  function wrapper(this: ThisParameterType<T>, ...args: Parameters<T>) {
-    if (isThrottle) {
-      savedArgs = args
-      savedThis = this
-
-      return
+  return (...args) => {
+    if (!isThrottle) {
+      isThrottle = true
+      setTimeout(() => (isThrottle = false), ms)
+      lastResult = fn(args)
     }
 
-    fn.apply(this, args)
-
-    isThrottle = true
-
-    setTimeout(() => {
-      isThrottle = false
-
-      if (savedArgs !== null && savedThis !== null) {
-        wrapper.apply(savedThis!, savedArgs)
-
-        savedArgs = null
-        savedThis = null
-      }
-
-    }, delay)
+    return lastResult
   }
-
-  return wrapper
 }
