db75b191209667d66ed46cb869b5557efcf14270
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-06-14 19:22:48 +0800

    feat: rework locationObserver

--- src/location-observer.ts
+++ src/location-observer.ts
@@ -1,49 +1,34 @@
 type LocationCallback<T = any> = (location: Location, args: T) => void
 
 type Events<T> = {
-  pushState: LocationCallback<T>
-  replaceState: LocationCallback<T>
-  popState: LocationCallback<
-    Omit<PopStateEvent, 'state'> & { readonly state: T }
-  >
+  onPush: LocationCallback<T>
+  onReplace: LocationCallback<T>
 }
 
 /**
- * Observe changes to the location
+ * Observes changes to the browser's location and history, and invokes
+ * the specified callbacks when the user navigates to a new page or updates the
+ * current page's state.
+ *
+ * @param {Events<T>} events - An object that contains optional callback functions
+ * for push and replace actions.
  * @example
- * const observer = new LocationObserver<{ id: string }>()
- * observer.on('pushState', (location, state) => {
- *   console.log(state.id)
+ * locationObserver<{ id: string }>({
+ *   onPush: (location, args) => {},
+ *   onReplace: (location, args) => {}
  * })
  */
-export class LocationObserver<T> {
-  #events = {} as Record<keyof Events<T>, LocationCallback<any>>
+export function locationObserver<T>(events: Events<T>): void {
+  const { history, location } = window
+  const { pushState, replaceState } = history
 
-  constructor() {
-    const { history, location } = window
-    const { pushState, replaceState } = history
-
-    history.pushState = (...args) => {
-      pushState.apply(history, args)
-      this.#events?.pushState(location, args[0])
-    }
-
-    history.replaceState = (...args) => {
-      replaceState.apply(history, args)
-      this.#events?.replaceState(location, args[0])
-    }
-
-    window.addEventListener('popstate', (event) => {
-      this.#events?.popState(location, event)
-    })
+  history.pushState = (...args) => {
+    pushState.apply(history, args)
+    events.onPush(location, args[0])
   }
 
-  on<E extends keyof Events<T>>(event: E, listener: Events<T>[E]): () => void {
-    this.#events[event] = listener
-    return () => this.off(event)
-  }
-
-  off<E extends keyof Events<T>>(event: E): void {
-    delete this.#events[event]
+  history.replaceState = (...args) => {
+    replaceState.apply(history, args)
+    events.onReplace(location, args[0])
   }
 }
--- test/location-observer.test.ts
+++ test/location-observer.test.ts
@@ -1,46 +1,26 @@
 import { describe, expect, expectTypeOf } from 'vitest'
-import { LocationObserver } from '../src/location-observer.js'
+import { locationObserver } from '../src/location-observer.js'
 
-describe('LocationObserver', (test) => {
-  const locationObserver = new LocationObserver<{ id: number }>()
-
+describe('locationObserver', (test) => {
   test('should be defined', () => {
     expect(locationObserver).toBeDefined()
   })
 
-  test('pushState', () => {
-    locationObserver.on('pushState', (location, state) => {
-      expectTypeOf(state).toEqualTypeOf<{ id: number }>()
-      expect(state).toEqual({ id: 1 })
-      expect(location.pathname).toBe('/foo')
+  test('pushState/replaceState', () => {
+    locationObserver<{ id: number }>({
+      onPush(location, args) {
+        expectTypeOf(args).toEqualTypeOf<{ id: number }>()
+        expect(args).toEqual({ id: 1 })
+        expect(location.pathname).toBe('/foo')
+      },
+      onReplace(location, args) {
+        expectTypeOf(args).toEqualTypeOf<{ id: number }>()
+        expect(args).toEqual({ id: 2 })
+        expect(location.pathname).toBe('/bar')
+      }
     })
 
     history.pushState({ id: 1 }, '', '/foo')
-    locationObserver.off('pushState')
-  })
-
-  test('replaceState', () => {
-    locationObserver.on('replaceState', (location, state) => {
-      expectTypeOf(state).toEqualTypeOf<{ id: number }>()
-      expect(state).toEqual({ id: 2 })
-      expect(location.pathname).toBe('/bar')
-    })
-
     history.replaceState({ id: 2 }, '', '/bar')
-    locationObserver.off('replaceState')
-  })
-
-  test('popState', () => {
-    locationObserver.on('popState', (location, event) => {
-      expect(event.state).toEqual({ id: 3 })
-      expect(location.pathname).toBe('/bar')
-    })
-
-    window.dispatchEvent(
-      new PopStateEvent('popstate', {
-        state: { id: 3 }
-      })
-    )
-    locationObserver.off('popState')
   })
 })
